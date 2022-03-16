4a47e8c41a20cb531fba6aa0bb1fee8766f7f4d6
Mateusz Wieczorek <mateusz.wu@gmail.com>
2022-03-16 11:09:50 +0100

    f1 - add teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Team.create(name: 'Arsenal F.C.')
+Team.create(name: 'Real Madrid')
+Team.create(name: 'FC Barcelona')

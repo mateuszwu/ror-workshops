fe88bc0a06c4addfca7f43e82d04042d1bdfc7ca
Mateusz Wieczorek <mateusz.wu@gmail.com>
2022-03-18 13:43:53 +0100

    f13 - user can see each Round result

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def show
     @users = User.all.includes(:bets).sort_by(&:total_points).reverse
+    @pagy, (@round,) = pagy(Round.order(id: :desc), items: 1)
   end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,3 +1,5 @@
 class Bet < ApplicationRecord
+  belongs_to :match
+
   validates :home_team_score, :away_team_score, presence: true
 end

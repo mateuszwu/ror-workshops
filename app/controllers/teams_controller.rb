9bd1a655634a20d4335c647e4125db7c7e44306c
PawelMolek <pawel.molek@gmail.com>
2022-03-30 18:28:23 +0200

    Team Controller created, added views, added model

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,52 @@
+class TeamsController < ApplicationController
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to teams_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+
+    if @team.update(team_params)
+      redirect_to @team
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+    private
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "teams#index"
+  resources :teams
   # Defines the root path route ("/")
   # root "articles#index"
 end

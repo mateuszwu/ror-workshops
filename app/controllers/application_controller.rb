16084945598da56d0ba642d9fb9258c2586825c7
Azzerous <damiangrygiercz@gmail.com>
2022-03-31 18:51:18 +0200

    feature: add name and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name])
+  end
 end

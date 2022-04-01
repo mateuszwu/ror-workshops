cc2744306e77dcec57e1286272921aa90ef0246b
umburek <robert.kubica1982@gmail.com>
2022-04-01 19:13:00 +0200

    Added User register/login/logout

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
+  belongs_to :user
   has_one_attached :avatar
   validates :name, presence: true
 end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :teams
+  has_one_attached :user_avatar
+end

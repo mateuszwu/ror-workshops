87bc8849870fc1685d0f45a08bfb828600d551d3
Lara500 <sonia8835@interia.eu>
2022-04-01 17:51:40 +0200

    add_homePage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :teams
   resources :users, only: %i[show edit update]
+  resource :home
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'teams#index'
+  root 'home#show'
 end

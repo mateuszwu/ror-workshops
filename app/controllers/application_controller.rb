72091eaccfc7654c3754cf35e4dce1fa1d60c48d
Azzerous <damiangrygiercz@gmail.com>
2022-04-01 18:03:12 +0200

    Feature: only admin users can manage (browse & modify) Teams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+  def is_admin?
+    unless current_user.is_admin
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,6 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :is_admin?
 
   # GET /teams
   def index

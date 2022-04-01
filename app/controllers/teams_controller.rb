88cc56384f67230463cee8b8b19557fa2762cc99
jacekszmidt <jszmidt2@gmail.com>
2022-04-01 16:27:46 +0200

    avatar

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -56,5 +56,6 @@ class TeamsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def team_params
     params.require(:team).permit(:name)
+    params.require(:team).permit(:avatar)
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   validates :name, presence: true
+  validates :avatar, presence: true
 end

f34138efed19eb4edffdf609075c6d2271ac472f
PawelMolek <pawel.molek@gmail.com>
2022-04-04 19:38:56 +0200

    editing and selecting matches works correctly

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,6 @@ class Match < ApplicationRecord
   validates :match_date, presence: true
 
   belongs_to :round
+  belongs_to :home_team, class_name: 'Team'
+  belongs_to :away_team, class_name: 'Team'
 end

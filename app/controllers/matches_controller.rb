bdfee2b83258dac71d3179436632f8c376eb1473
Marcin Kozieł <greebo4pl@gmail.com>
2022-04-04 18:49:42 +0200

    Match show and edit

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,6 @@
 class MatchesController < ApplicationController
     before_action :set_round
-    
+    before_action :set_match, only: [:edit, :update]
 
     def new
         @match= @round.matches.new
@@ -9,9 +9,20 @@ class MatchesController < ApplicationController
         @match = @round.matches.build(match_params)
     
         if @match.save
-          redirect_to round_url(@round), notice: 'match was successfully created.'
+            redirect_to round_url(@round), notice: 'match was successfully created.'
         else
-          render :new, status: :unprocessable_entity
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit  
+    end
+
+    def update
+        if @match.update(match_params)
+          redirect_to @round, notice: 'match was successfully updated.'
+        else
+          render :edit, status: :unprocessable_entity
         end
       end
 
@@ -20,6 +31,10 @@ class MatchesController < ApplicationController
   def set_round
     @round = Round.find(params[:round_id])
   end
+  def set_match
+    @match = Match.find(params[:id])
+  end
+
   def match_params
     params.require(:match).permit(:date)
   end
--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -9,6 +9,7 @@ class RoundsController < ApplicationController
 
   # GET /rounds/1
   def show
+    @matches = Match.all
   end
 
   # GET /rounds/new

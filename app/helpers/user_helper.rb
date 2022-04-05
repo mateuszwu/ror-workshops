e6c05121c8681409a8a19a23f7a07586431af474
umburek <robert.kubica1982@gmail.com>
2022-04-05 15:59:36 +0200

    Added User full name view option in navbar

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,4 +4,12 @@ module UserHelper
       image_tag current_user.avatar
     end
   end
+
+  def full_name(current_user)
+    if current_user.first_name.present? or current_user.last_name.present?
+      current_user.first_name + " " + current_user.last_name
+    else
+      current_user.email
+    end
+  end
 end

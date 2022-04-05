39d8ee3f0b15ca5a307debe1f63c895680c4a883
wielopolski <wielopolskim@gmail.com>
2022-04-05 11:55:20 +0200

    adding/editing team have to be another

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,15 @@
 class Match < ApplicationRecord
+    validate :expiration_teams_is_another
     belongs_to :round
     belongs_to :home_team, class_name: "Team"
     belongs_to :away_team, class_name: "Team"
     validates :date, presence: true
 
     has_many :teams
+
+    def expiration_teams_is_another
+      if home_team_id == away_team_id
+        errors.add(:home_team,"have to play with another  team")
+      end
+    end
   end

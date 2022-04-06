3aa7c791db1c51a7decec82560d4b91af8c9f888
domix1996 <dominiknarozynski@gmail.com>
2022-04-06 16:45:32 +0200

    add few elem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_132803) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_153350) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_132803) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bets", force: :cascade do |t|
+    t.integer "match_id_id", null: false
+    t.integer "user_id_id", null: false
+    t.integer "home_team_score"
+    t.integer "away_team_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id_id"], name: "index_bets_on_match_id_id"
+    t.index ["user_id_id"], name: "index_bets_on_user_id_id"
   end
 
   create_table "matches", force: :cascade do |t|
@@ -83,6 +94,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_132803) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bets", "matches", column: "match_id_id"
+  add_foreign_key "bets", "users", column: "user_id_id"
   add_foreign_key "matches", "rounds"
   add_foreign_key "matches", "teams", column: "away_team_id"
   add_foreign_key "matches", "teams", column: "home_team_id"

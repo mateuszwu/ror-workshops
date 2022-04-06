1d72dcdd0b3fdd025162d2e39562b06b9cce9a46
Marcin Kozieł <greebo4pl@gmail.com>
2022-04-06 17:25:35 +0200

    Home shows leaderboard with sorted points

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+
+
+def show 
+    @users = User.all.sort_by(&:points).reverse
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :bets
   has_one_attached :avatar
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def points
+    bets.sum(:points)
   end
 end

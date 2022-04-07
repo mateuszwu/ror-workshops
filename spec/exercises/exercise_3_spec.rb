8138c37756a5fc682b09dab0549743153f616a5b
Marcin Kozieł <greebo4pl@gmail.com>
2022-04-07 18:40:46 +0200

    looks fine

--- spec/exercises/exercise_3_spec.rb
+++ spec/exercises/exercise_3_spec.rb
@@ -13,8 +13,18 @@
 
 require 'rails_helper'
 
-def valid_round
-  Round.create(year: 2022, number:1, created_at:Date.today, updated_at:Date.today)
+# def valid_round
+#   Round.create(year: 2022, number:1, created_at:Date.today, updated_at:Date.today)
+# end
+
+class RoundsFactory
+  @@next_id = 0
+  
+  def self.build(year, number)
+  @@next_id += 1
+  date = Date.parse('2077-01-04')
+    Round.new(year: year, number: number, id: @@next_id, created_at: date, updated_at: date)
+  end
 end
 
 class TeamsFactory
@@ -30,18 +40,13 @@ RSpec.describe Match do
   describe '#valid?' do
     it 'return false when home team is the same as away team' do  
       team = TeamsFactory.build(name: "Szermierka Kielce")
-      expect(Match.new(id: 1, match_date:Date.today, round: valid_round, home_team: team, away_team: team)).to_not be_valid 
+      expect(Match.new(id: 1, match_date:Date.today, round: RoundsFactory.build(2022, 1), home_team: team, away_team: team)).to_not be_valid 
     end
 
     it 'return true when home team is diffrent than the away team' do  
       team1 = TeamsFactory.build(name: "Szermierka Kielce")
       team2 = TeamsFactory.build(name: "Rebel Fencing")
-      expect(Match.new(id: 1, match_date:Date.today, round: valid_round, home_team: team1, away_team: team2)).to be_valid 
+      expect(Match.new(id: 1, match_date:Date.today, round: RoundsFactory.build(2022, 1), home_team: team1, away_team: team2)).to be_valid 
     end
   end
 end
-
-# 1) Match#valid? return true when home team is diffrent than the away team
-# Failure/Error: expect(Match.new(round: valid_round, home_team: team1, away_team: team2)).to be_valid
-#   expected #<Match id: nil, home_team_id: 1, away_team_id: 2, away_team_score: nil, home_team_score: nil, 
-#   round_id: 1, match_date: nil, created_at: nil, updated_at: nil> to be valid, but got errors: Match date can't be blank
